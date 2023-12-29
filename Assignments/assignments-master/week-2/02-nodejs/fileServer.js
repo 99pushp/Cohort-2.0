b6a92fd878733ab9dfc0a138908b58dde303388a
pu5hp <pushpdeepsinghv@gmail.com>
2023-12-29 20:37:55 +0530

    file_server on express assingment done

--- Assignments/assignments-master/week-2/02-nodejs/fileServer.js
+++ Assignments/assignments-master/week-2/02-nodejs/fileServer.js
@@ -16,6 +16,70 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const port = 3000;
 
+const files_folder_path = '/home/pushp/Documents/Cohort 2.0/Assignments/assignments-master/week-2/02-nodejs/files/';
+let arrayofValidfiles = {};
+
+
+
+function pathchecker(str){
+for(let i=0;i<arrayofValidfiles.length;i++){
+  if(str == arrayofValidfiles[i])
+  {
+  return true;
+  }
+  }
+return false;
+}
+
+
+
+
+//2.GET
+app.get('/file/:fileName',(req,res)=>{
+
+//logic if file is in directory
+
+        let fileName = req.params.fileName;
+        console.log('filename:',fileName);
+        
+        if(pathchecker(fileName)){
+        let filepath = path.join(files_folder_path,fileName);
+        console.log('filepath:',filepath);
+        fs.readFile(filepath,'utf-8',(err,data)=>{
+          res.status(200).send(data);
+        })
+        }
+        else{
+        //res.send("File not found");
+        res.status(404).send("file not found");
+        }
+})
+
+
+
+
+
+// 1.GET
+app.get('/files',(req,res)=>{
+  
+  arrayofValidfiles = fs.readdirSync(files_folder_path);
+  console.log(typeof(arrayofValidfiles));
+  console.log(arrayofValidfiles);
+  for(let i=0;i<arrayofValidfiles.length;i++){
+  console.log(arrayofValidfiles[i]);
+  }
+  res.send(fs.readdirSync(files_folder_path));//returns object
+})
+
+
+
+
+
+//starting the server
+app.listen(port,function(){
+  console.log('server started at port: '+ port);
+})
 
 module.exports = app;
