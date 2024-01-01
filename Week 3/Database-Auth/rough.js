9a6cf38dfc23a6ed345c8e35a82e01c511e4a5eb
pu5hp <pushpdeepsinghv@gmail.com>
2024-01-01 23:18:40 +0530

    adding fetch api JWT DATABASES

---
+++ Week 3/Database-Auth/rough.js
@@ -0,0 +1,19 @@
+const ALL_USERS = [
+    {
+      username: "harkirat@gmail.com",
+      password: "123",
+      name: "harkirat singh",
+    },
+    {
+      username: "raman@gmail.com",
+      password: "123321",
+      name: "Raman singh",
+    },
+    {
+      username: "priya@gmail.com",
+      password: "123321",
+      name: "Priya kumari",
+    },
+  ];
+
+console.log(ALL_USERS[0].name);
