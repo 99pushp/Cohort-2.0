70f2a6677372eb6e78719ac7c91fd514112b407f
pu5hp <pushpdeepsinghv@gmail.com>
2024-01-09 10:52:12 +0530

    AUTH ADDED TO COURSE SELLING APP

--- Assignments/assignments-master/week-3/04-mongo-with-jwt-auth/db/index.js
+++ Assignments/assignments-master/week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,32 @@
+//connection is made here and schemas are defined here
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://pu5hp:qU5ZDnDjn9wE9kSS@cluster0.l8hqvdz.mongodb.net/Courses-Assignment-Week-4');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    price:Number
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- Assignments/assignments-master/week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ Assignments/assignments-master/week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,85 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
-
+const {Admin} = require("../db");
+const JWT_SECRET = "secret";
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    //create in db
+    Admin.create({
+        username : username,
+        password: password
+    })
+    .then(()=>{
+        res.status(200).json({
+            msg:'Admin created successfully'
+        })
+    })
 });
 
 router.post('/signin', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+       
+    const user = User.find({
+        username,
+        password
+    })
+    user.then(()=>{
+        if(user){
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+    
+        res.json({
+            token:token
+        })}else{
+            res.status(411).json({
+                message:"Incorrect email and password"
+            })
+        }
+    })
+
+    
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+   
+
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageLink = req.body.imageLink;
+
+    let newCourse = Course.create({
+        title:title,
+        description:description,
+        price:price,
+        imageLink:imageLink
+    })
+    newCourse.then((response)=>{
+        res.json({
+            message : 'course created successfully', 
+            courseId : response._id
+        })
+    })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+  
+
+    let courses = Course.find({});
+    courses.then((response)=>{
+        res.status(200).json({
+            courses : response
+        })
+    })
 });
 
 module.exports = router;
